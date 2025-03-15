9cbf96a36797341994956f55e58ba3c4e1ec69fd
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-03-15 12:19:23 +0200

    Update Identifiable.java

--- src/Domain/Identifiable.java
+++ src/Domain/Identifiable.java
@@ -3,6 +3,7 @@ package Domain;
 import java.io.Serializable;
 
 public interface Identifiable<T> extends Serializable {
+    //all of our Domain classes will implement this Identifiable interface, to ensure each class instance is identified in the same way.
     public T getId();
     public void setId(T id);
 }
